6641f0a84f5d43d4326f1a1a97bd99bd8b72fe37
rehman-m <302485736@student.rocmondriaan.nl>
2022-09-05 12:47:34 +0200

    ZUZU WEBSITE HOME PAGINA GEMAAKT

---
+++ sushi.php
@@ -0,0 +1,89 @@
+<!DOCTYPE>
+
+<html>
+<head> <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid bg-dark">
+    <ul class="nav nav-fill">
+
+        <li class="nav-item">
+            <a class="nav-link text-light " role="button">ZUZU </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-light " role="button">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-light" role="button">Bestellen</a>
+        </li>
+
+    </ul>
+
+</div>
+
+<div class= "container-fluid">
+    <div>
+        <div class="col-12">
+            <img src="imgs/vertical.jpg" height= 25% width=100%>
+        </div>
+    </div>
+</div>
+
+<div class="container-md ">
+    <header class=" fs-1 text-center mt-5" >Goedemiddag, welkom bij ZuZu </header>
+</div>
+<div class="container-md">
+    <p class=" text-center ">Wij zijn gespecialiseerd in de Japnese keuken.<br> Het Woord "sushi is afkomstig van "su",wat azijn betekent. en "shi" Rijst.</p>
+</div>
+<div class="container-md">
+    <p class="fs-5 text-center "> Vandaag dinsdag 23 augustus 2022 <br> Bezorgtijd vanaf nu :18:00</p>
+</div>
+
+
+    <div class="container-md ">
+        <div class="row">
+
+            <div class="col-6" >
+
+                <div class="card" width=20%>
+                    <img class="  ms-5 " src="imgs/sushi-3.jpg" width=550px>
+                    <div class="card-body">
+                        <h5 class="card-title ms-5 fst-italic">Bestel bij ons je sushi's</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6" >
+
+                <div class="card" width=20%>
+                    <img class="  ms-5 " src="imgs/sushi-3.jpg" width=550px>
+                    <div class="card-body">
+                        <h5 class="card-title ms-5 fst-italic">Bestel bij ons je sushi's</h5>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </div>
+<div class="container-fluid bg-black  p-5 ">
+    <div class-="row">
+       <div class="col-4 text-light ms-5">Contact gegevens</div>
+    <div class="col-4 text-light ms-5">Restaurant ZuZu</div>
+    <div class="col-4 text-light ms-5">Appelstraat 1</div>
+    <div class="col-4 text-light ms-5">1111A Fruit</div>
+    <div class="col-4 text-light ms-5"> ZuZu@gmail.com</div>
+
+
+    </div>
+
+
+</div>
+
+</body>
+</html>
+
+<?php
